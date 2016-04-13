454e3104b205543408c47356b2690db3cfd05abf
simmibadhan <simmi@addvalsolutions.com>
2016-04-13 15:08:33 +0530

    folder structure changes

---
+++ app/ts/app.js
@@ -0,0 +1,37 @@
+System.register(["angular2/platform/browser", "angular2/core"], function(exports_1) {
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var browser_1, core_1;
+    var Ng2App;
+    return {
+        setters:[
+            function (browser_1_1) {
+                browser_1 = browser_1_1;
+            },
+            function (core_1_1) {
+                core_1 = core_1_1;
+            }],
+        execute: function() {
+            Ng2App = (function () {
+                function Ng2App() {
+                }
+                Ng2App = __decorate([
+                    core_1.Component({
+                        selector: 'ng2app',
+                        template: "\n    <h2> Angular 2 Practice App </h2>\n  "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], Ng2App);
+                return Ng2App;
+            })();
+            browser_1.bootstrap(Ng2App);
+        }
+    }
+});
